98a383fb55dfe8ffa3c63a838d9889aaeffd9c20
YinYangSV <oscarwallin02@hotmail.com>
2026-05-16 11:15:40 +0200

    create_users.sh

---
+++ create_users.sh
@@ -0,0 +1,22 @@
+#!/bin/bash'
+if [ "$EUID" -ne 0 ]; then
+    echo "Kör som root!"
+fi
+
+for user in "$@" ; do
+    echo "Söker användare för $user"
+    id "$user" &>/dev/null
+if [ $? -eq 0 ]; then
+    echo "Användaren $@ finns redan i systemet"
+    continue
+fi
+
+useradd -m "$user"
+mkdirm /home/$user/Documents /home/$user/Downloads /home/$user/Work
+chmod 700 /home/$user/Documents /home/$user/Downloads /home/$user/Work
+chown -R $user:$user /home/$user
+echo "Välkommen $user" > /home/$user/welcome.txt
+cut -d: -f1 /ect/passwd | grep -v "^$user$" >> /home/$user/welcome.txt
+cat /home/$user/welcome.txt
+
+done
