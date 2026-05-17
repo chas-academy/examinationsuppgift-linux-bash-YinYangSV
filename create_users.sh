75b9f9339d87d790cde027f7b2c27261603449ea
YinYangSV <oscarwallin02@hotmail.com>
2026-05-17 15:47:55 +0200

    create_users.sh

--- create_users.sh
+++ create_users.sh
@@ -8,7 +8,7 @@ fi
 for user in "$@" ; do
     echo "Söker användare för $user"
     id "$user" &>/dev/null
-if [ $? -eq 0 ]; then
+if [ $user -eq 0 ]; then
     echo "Användaren $user finns redan i systemet"
     continue
 fi
@@ -19,6 +19,7 @@ mkdir /home/$user/Downloads
 mkdir /home/$user/Work
 mkdir /home/$user/Documents
 
+chmod 700 /home/$user
 chmod 700 /home/$user/Downloads
 chmod 700 /home/$user/Work
 chmod 700 /home/$user/Documents
@@ -27,8 +28,9 @@ echo "Välkommen $user" > /home/$user/welcome.txt
 
 cut -d: -f1 /etc/passwd | grep -v "^$user$" >> /home/$user/welcome.txt
 
+chown -R $user:$user /home/$user
+
 cat /home/$user/welcome.txt
 
-chown -R $user:$user /home/$user
 
 done
